780b49224043672f775c5b6349c604e91be47e1e
Balaji Umapathy <jaswanthbalamala992004@gmail.com>
2024-08-19 20:58:49 +0530

    first commit

---
+++ src/data/FeaturesData.js
@@ -0,0 +1,63 @@
+// blog data
+
+export const Features
+ =[
+    {
+        "id": 1,
+        "name": "HTML and CSS in Depth",
+        "tags": [
+            "Cascading Style Sheets (CSS)",
+            "HTML5",
+            "Bootstrap (Framework)"
+        ],
+        "imgSrc": "https://res.cloudinary.com/dx5alvq9t/image/upload/v1724046797/CERTIFICATE_LANDING_3_alow0d.jpg",
+        "link": "https://coursera.org/share/75f035baea3949f9057724351cc0df40",
+        "issuedBy": "Meta"
+    },
+    {
+        "id": 2,
+        "name": "Programming with JavaScript",
+        "tags": [
+            "JavaScript"
+        ],
+        "imgSrc": "https://res.cloudinary.com/dx5alvq9t/image/upload/v1724046797/CERTIFICATE_LANDING_PAGE_CP27WMFQDZ78_1_yuifof.jpg",
+        "link": "https://coursera.org/share/2b0c3f498d15fd8aead67804d64b4faf",
+        "issuedBy": "Meta"
+    },
+    {
+        "id": 3,
+        "name": "Version Control",
+        "tags": [
+            "Git",
+            "GitHub"
+        ],
+        "imgSrc": "https://res.cloudinary.com/dx5alvq9t/image/upload/v1724046797/CERTIFICATE_LANDING_PAGE_PSJCGLLQVYYI_2_gsfb8f.jpg",
+        "link": "https://coursera.org/share/e46e58ce9f3b9f1e823059a9aa4a7188",
+        "issuedBy": "Meta"
+    },
+    {
+        "id": 4,
+        "name": "Introduction to Front-End Development",
+        "tags": [
+            "Front-End Development",
+            "HTML5",
+            "Cascading Style Sheets (CSS)",
+            "Web Development",
+            "Bootstrap (Framework)"
+        ],
+        "imgSrc": "https://res.cloudinary.com/dx5alvq9t/image/upload/v1724046798/CERTIFICATE_LANDING_PAGE_F5LZYSAPTK7W_hz9ktx.jpg",
+        "link": "https://coursera.org/share/e5a48a78db55a0160b726688f4751522",
+        "issuedBy": "Meta"
+    },
+    {
+        "id": 5,
+        "name": "Video Editor",
+        "tags": [
+            "Video Editing",
+            "DaVinci Resolve"
+        ],
+        "imgSrc": "https://res.cloudinary.com/dx5alvq9t/image/upload/v1724049137/rook_logo_fcoxiv.png",
+        "link": "https://res.cloudinary.com/dx5alvq9t/image/upload/v1724048308/1708487311331_hd3nun.jpg",
+        "issuedBy": "3 months intership in Rook"
+    }
+]
